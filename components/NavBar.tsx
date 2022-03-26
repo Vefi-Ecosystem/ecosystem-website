40695fad78c4421b95fe6b0fa8a8d30b94173263
anie308 <aniebietabasiobo@gmail.com>
2022-03-26 21:14:44 +0100

    commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,99 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Logo from '../assets/vefilogo.png'
+import Slash from '../assets/slash.png'
+import ThemeSwitcher from './ThemeSwitcher'
+import { BiMenuAltLeft} from 'react-icons/bi'
+import { FaTimes} from 'react-icons/fa'
+import React, { useState } from 'react'
+import
+{
+  Div,
+  Div1,
+    Div2,
+    Div3,
+    Nav,
+    NavLinks,
+    Sec
+}
+
+ from '../styles/NavBar.styles'
+
+ type DivProps= {
+  click : boolean
+}
+
+
+
+function NavBar() {
+
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+
+  
+  return (
+    
+    <Nav >
+      
+      <Div click={click}>
+        
+        <NavLinks onClick={handleClick} >
+          <div className='icon-pack'>
+            <FaTimes/>
+          </div>
+          <div className='link'>
+           <Link href='/'>
+                Home
+           </Link>
+          </div>
+          <div className='link'>
+           <Link href='/'>
+                Partners
+           </Link>
+          </div>
+          <div className='link'>
+           <Link href='/'>
+                Products
+           </Link>
+          </div>
+          <div className='link'>
+           <Link href='/'>
+                Team
+           </Link>
+          </div>
+          
+        
+        </NavLinks>
+      </Div>
+   <Sec>
+     <Div1>
+       <BiMenuAltLeft className='icon' onClick={handleClick}/>
+     </Div1>
+
+     <Div2>
+       <Link href='/'>
+       <a>
+       <Image src={Logo}/>
+       </a>
+       </Link>
+      </Div2>
+   
+      <Div3>
+      <div className='join'>
+     <div> Join our community</div>
+     <div className='slash'>
+       <Image src={Slash}/>
+     </div>
+      </div>
+       <ThemeSwitcher/>
+      </Div3>
+      </Sec>
+
+
+      
+    </Nav>
+  )
+}
+
+export default NavBar
---
+++ styles/NavBar.styles.tsx
@@ -0,0 +1,164 @@
+import styled from "styled-components";
+
+type DivProps= {
+    click : any
+ }
+
+export const Nav = styled.div` 
+display:flex ;
+align-items:c ;
+justify-content: space-between ;
+background: rgba(238, 238, 238, 1) ;
+margin: 0;
+padding: 20px 40px;
+box-shadow: 0px 4px 26px rgba(0, 0, 0, 0.09);
+
+
+
+@media screen and (max-width: 1000px){
+    flex-direction:column ;
+    padding:0px ;
+   
+}
+`
+export const Div = styled.div<DivProps>` 
+transition: all  0.5s ease-out;
+@media screen and (max-width: 1000px){
+  
+    position:fixed ;
+    top: 0;
+    /* left: ${({ click }) => (click ? 0 : '-100%')}; */
+    left: ${({click}) => (click? 0: '-100%')};
+
+display:flex ;
+    flex-direction:column ;
+    z-index:1 ;
+    background: #EDEDED;
+box-shadow: 5px 5px 7px 4px rgba(0, 0, 0, 0.06);
+height:90vh ;
+width:174px ;
+padding: 20px;
+
+}
+
+
+`
+export const NavLinks = styled.div`
+display:flex ;
+justify-content: space-between ;
+display: flex;
+.icon-pack{
+    width:100% ;
+    display:flex ;
+    align-items:flex-end ;
+    justify-content:flex-end ;
+    font-size: 25px;
+
+
+   
+}
+
+
+.link{
+    margin-right: 20px;
+    font-family:  poppins;
+    font-size: 16px;
+    line-height: 19px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    letter-spacing: -0.015em;
+    color: #333333;
+
+    @media screen and (max-width: 1000px){
+margin:20px 0 ;
+align-items:  flex-start;
+   
+}
+}
+
+@media screen and (max-width: 1000px){
+   flex-direction:column ;
+   align-items:none ;
+
+    /* justify-content:flex-end ; */
+   
+}
+`
+export const Sec = styled.div`
+
+    width:100% ;
+    display:flex ;
+    justify-content:space-between ;
+    align-items:center ;
+
+
+
+    @media screen and (max-width: 1000px){
+        padding:20px ;
+        justify-content:space-between ;
+        flex:none ;
+   
+
+}
+    
+    
+`
+export const Div1 = styled.div`
+    display: none;
+
+
+    @media screen and (max-width: 900px){
+        display:flex ;
+
+
+        .icon{
+            font-size: 34px;
+            color: #1D1D1D; 
+        }
+    }
+`
+export const Div2 = styled.div` 
+margin-left: 300px;
+
+@media screen and (max-width: 800px){
+    margin:0 ;
+}
+
+`
+export const Div3 = styled.div` 
+/* flex:2 ; */
+
+
+display:flex ;
+align-self:center ;
+
+@media screen and (max-width: 1000px){
+     
+   
+}
+
+
+
+.join{
+    font-size: 16px;
+line-height: 19px;
+display: flex;
+flex-direction:column ;
+align-items: center;
+text-align: center;
+letter-spacing: -0.015em;
+margin-right:40px ;
+
+color: #333333;
+
+@media screen and (max-width: 1000px){
+       display:none ;
+   
+}
+
+.slash{
+}
+}
+`
+
