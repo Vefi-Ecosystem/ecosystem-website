d90534634873cffaf67f5f0c296e180baf64d564
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-03-31 09:05:53 +0100

    added GlobalContextProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic';
+
+
+const GlobalContextProvider = dynamic(
+  () => import('../context/globalContext'),
+  {ssr:false}
+)
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+  return (
+    <>
+      <GlobalContextProvider>
+          <Component {...pageProps} />
+      </GlobalContextProvider>
+    </>
+  )
 }
 
 export default MyApp
