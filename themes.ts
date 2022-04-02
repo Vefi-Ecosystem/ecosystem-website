07e393bbd4638e1c49139bc5657ab403e9a3fe9b
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-04-02 16:17:43 +0100

    updated link color theme

--- themes.ts
+++ themes.ts
@@ -1,21 +1,21 @@
 export const darkTheme = {
   backgroundColor: '#1D1D1D',
   headerTextColor: '#B8B8B8',
-  linkColor: '#DDDDDD',
   regularTextColor: '#8F8F8F',
   svgIconColor: '#DDDDDD',
   themeSwitchBtn: '#1D1D1D',
   themeSwitchShadow: 'inset 0px 4px 7px 4px rgba(0, 0, 0, 0.8)',
-  themeSwitchBackground: '#161616'
+  themeSwitchBackground: '#161616',
+  linkColor:"#DDDDDD"
 };
 
 export const lightTheme = {
   backgroundColor: '#E5E5E5',
   headerTextColor: '#000000',
-  linkColor: '#333333',
   regularTextColor: '#333333',
   svgIconColor: '#1D1D1D',
   themeSwitchBtn: '#EEEEEE',
   themeSwitchShadow: 'inset 0px 4px 7px 4px #DDDDDD',
-  themeSwitchBackground: '#EEEEEE'
+  themeSwitchBackground: '#EEEEEE',
+  linkColor:"#1D1D1D"
 };
