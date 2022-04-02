ad18f725f0dce45ca1ef02ccbd470b45fc59be29
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-04-02 17:14:51 +0100

    added more themes

--- themes.ts
+++ themes.ts
@@ -6,7 +6,9 @@ export const darkTheme = {
   themeSwitchBtn: '#1D1D1D',
   themeSwitchShadow: 'inset 0px 4px 7px 4px rgba(0, 0, 0, 0.8)',
   themeSwitchBackground: '#161616',
-  linkColor:"#DDDDDD"
+  linkColor:"#DDDDDD",
+  jumpArrowbgColor:"#1D1D1D",
+  jumpArrowShadow:'0px 9px 12px rgba(0, 0, 0, 0.26)',
 };
 
 export const lightTheme = {
@@ -17,5 +19,7 @@ export const lightTheme = {
   themeSwitchBtn: '#EEEEEE',
   themeSwitchShadow: 'inset 0px 4px 7px 4px #DDDDDD',
   themeSwitchBackground: '#EEEEEE',
-  linkColor:"#1D1D1D"
+  linkColor:"#1D1D1D",
+  jumpArrowbgColor:"#F2F2F2",
+  jumpArrowShadow:'0px 9px 12px rgba(0, 0, 0, 0.16)',
 };
