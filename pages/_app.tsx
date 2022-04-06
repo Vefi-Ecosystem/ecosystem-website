ade4e1ae8c2f7afc2dee4ba57f2b4391199f7cf5
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-04-06 17:24:13 +0100

    restored pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,25 @@
-import type { AppProps } from 'next/app';
+
+import type { AppProps } from 'next/app'
 
 import dynamic from 'next/dynamic';
 
-const GlobalContextProvider = dynamic(() => import('../context/globalContext'), { ssr: false });
+
+const GlobalContextProvider = dynamic(
+  () => import('../context/globalContext'),
+  {ssr:false}
+)
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+
   return (
     <>
       <GlobalContextProvider>
-        <Component {...pageProps} />
+          <Component {...pageProps} />
       </GlobalContextProvider>
     </>
-  );
+  )
 }
 
 export default MyApp;
