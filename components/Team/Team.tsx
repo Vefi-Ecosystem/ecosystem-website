21fb1ce3f6e41459a024f8cf6a14241a495fb7ea
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-04-06 01:41:28 +0100

    fixed team card

--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -4,83 +4,86 @@ import James from '../../assets/images/james.png'
 import Kingsley from '../../assets/images/kingsley.png'
 
 import {
-Container
+    Container
 }
-from '../../styles/Team.style'
+    from '../../styles/Team.style'
+
+const MemberCard = () => {
+
+}
 
 function Team() {
-  return (
-    <Container>
-        <div className='mother-con'>
-            <div className='img-card1'>
-                <div>
+    return (
+        <Container>
+            <div className='mother-con'>
+                <div className='img-card1'>
                     <div>
-                    <Image src={James}
-                    alt='Founding fathers'
-                    height={340}
-                    width={268}/>
-                    </div>
-                    <div className='mind-con'>
-                        <div className='mind-name'>
-                            James Emmanuel
+                        <div>
+                            <Image src={James}
+                                alt='Founding fathers'
+                                height={340}
+                                width={268} />
                         </div>
-                        <div className='post'>
-                            Chief Executive Officer
+                        <div className='mind-con'>
+                            <div className='mind-name'>
+                                James Emmanuel
+                            </div>
+                            <div className='post'>
+                                Chief Executive Officer
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <Image src={Kingsley} alt='Founding fathers' />
+                        </div>
+                        <div className='mind-con'>
+                            <div className='mind-name'>
+                                James Emmanuel
+                            </div>
+                            <div className='post'>
+                                Chief Executive Officer
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div>
-                <div>
-                    <Image src={Kingsley} alt='Founding fathers'/>
-                    </div>
-                    <div className='mind-con'>
-                        <div className='mind-name'>
-                            James Emmanuel
+                <div className='img-card2'>
+                    <div>
+                        <div>
+                            <Image src={James} alt='Founding fathers' />
                         </div>
-                        <div className='post'>
-                            Chief Executive Officer
+                        <div className='mind-con'>
+                            <div className='mind-name'>
+                                James Emmanuel
+                            </div>
+                            <div className='post'>
+                                Chief Executive Officer
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <Image src={James} alt='Founding fathers' />
+                        </div>
+                        <div className='mind-con'>
+                            <div className='mind-name'>
+                                James Emmanuel
+                            </div>
+                            <div className='post'>
+                                Chief Executive Officer
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='img-card2'>
-            <div>
-                    <div>
-                    <Image src={James} alt='Founding fathers'/>
-                    </div>
-                    <div className='mind-con'>
-                        <div className='mind-name'>
-                            James Emmanuel
-                        </div>
-                        <div className='post'>
-                            Chief Executive Officer
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                    <Image src={James} alt='Founding fathers'/>
-                    </div>
-                    <div className='mind-con'>
-                        <div className='mind-name'>
-                            James Emmanuel
-                        </div>
-                        <div className='post'>
-                            Chief Executive Officer
-                        </div>
-                    </div>
+            <div className='gen-card'>
+                <div className='big-text'>Genius minds <br /> behind the <br /> Vefi Ecosystem</div>
+                <div className='small-text'>
+                    Vefi Ecosystem comprises highly motivated individuals who are professionals with many years of experience in blockchain technology, business, economics, software engineering, etc.
                 </div>
             </div>
-        </div>
-        <div className='gen-card'>
-            <div className='big-text'>Genius minds <br/> behind the <br/> Vefi Ecosystem</div>
-            <div className='small-text'>
-            Vefi Ecosystem comprises highly motivated individuals who are professionals with many years of experience in blockchain technology, business, economics, software engineering, etc.
-            </div>
-
-        </div>
-    </Container>
-  )
+        </Container>
+    )
 }
 
 export default Team
