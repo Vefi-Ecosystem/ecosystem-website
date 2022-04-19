831dddd27d6abf4040a464b7d4edece10d0e8e01
promiselxg <okeydeede@gmail.com>
2022-04-19 09:06:45 +0100

    updated website description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import Head from 'next/head';
 import Layout from '../components/Layout';
 import NavBar from '../components/NavBar';
@@ -10,39 +10,29 @@ import Rev from '../components/Rev';
 import Chang from '../components/Chang';
 import Tabs from '../components/Tabs';
 
-
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div >
+      <div>
         <Head>
-          <title>Home Page  - Vefi ecosystem</title>
-          <meta name="description" content="Vefi ecosystem" />
+          <title>Vefi | Blockchain infrastructure and solution providing firm.</title>
+          <meta name="description" content="Vefi is building the future of web 3 for massive Blockchain adoption!" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <NavBar/>
-        
-        
+        <NavBar />
 
-        <main >
-        <Hero/>
-        <LogoSlide/>
-        <DescSec/>
-        <Addr/>
-        <Rev/>
-        <Chang/>
-        <Tabs/>
-
-         
-
-         
-          
+        <main>
+          <Hero />
+          <LogoSlide />
+          <DescSec />
+          <Addr />
+          <Rev />
+          <Chang />
+          <Tabs />
         </main>
-
-       
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
